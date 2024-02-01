deeb5c188781d01c306c0bd05451eca3baa7d667
Proprogam <k.kartal9595@gmail.com>
2024-02-01 09:23:36 +0300

    created:radix primitive

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import NavigationMenu from "@/components/Navbar/Navbar";
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <NavigationMenu />
+    </>
+  );
+};
+
+export default Home;
