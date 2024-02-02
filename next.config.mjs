0505330dee985d24013b428b65d68ac9d8fa7509
Proprogam <k.kartal9595@gmail.com>
2024-02-02 08:40:16 +0300

    fixed:githubPages css optimizasyon

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/nextjs-github-pages",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   *
+   */
 };
 
 export default nextConfig;
