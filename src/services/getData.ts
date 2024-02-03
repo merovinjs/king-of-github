b545ff14e4d0a5ce1fb6d40de5846da4d6168334
Proprogam <k.kartal9595@gmail.com>
2024-02-03 13:17:57 +0300

    fixed:hompage-bg-image

--- src/services/getData.ts
+++ src/services/getData.ts
@@ -1,1 +1,9 @@
-
+export async function getData() {
+  const options = {
+    headers: {
+      Authorization: `token ${process.env.GITHUB_KEY}`,
+    },
+  };
+  const jsstars = await fetch("https://api.github.com/search/repositories?q=language:javascript&sort=stars=desc", options);
+  return jsstars.json();
+}
