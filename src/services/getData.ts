7b93213d64a665ff649ca45831d21ff6efbc4a7f
Proprogam <k.kartal9595@gmail.com>
2024-02-03 11:47:40 +0300

    fixed:services components

--- src/services/getData.ts
+++ src/services/getData.ts
@@ -1,4 +1,1 @@
-export async function getData() {
-  const fetchJsstars = await fetch("https://api.github.com/search/repositories?q=language:javascript&sort=stars=desc&per_page=15");
-  return fetchJsstars.json();
-}
+
