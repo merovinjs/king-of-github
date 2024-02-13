3abf6fb708258dcef17d4938f9c3b2574c36b92d
Proprogam <k.kartal9595@gmail.com>
2024-02-13 09:24:32 +0300

    fixed test&service

--- tests/browser.test.ts
+++ tests/browser.test.ts
@@ -2,7 +2,7 @@ import { test } from "@playwright/test";
 
 test("test browser", async ({ page }) => {
   // point this to wherever you want
-  await page.goto("http://localhost:3000/");
+  await page.goto("https://king-of-github.vercel.app/");
 
   // keep browser open
   await page.pause();
